b8beb1d4a1ae4ec3583f2483baba0bd836836076
covoxS7 <lehmann.s7@gmx.de>
2022-12-02 23:27:06 +0100

    Projekt erstellt und Homepage gestaltet

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container-sm vh-100 bg-vue">
+    <div class="row h-100">
+      <div class="offset-1 col-10">
+        <div class="mt-5 text-center">
+          <div class="display-5">Flugzeug</div>
+          <hr />
+          <div class="display-1">Quiz</div>
+          <div class="mt-5">
+            <img src="@/assets/p47thunderbolt.svg" alt="" class="img-fluid" />
+          </div>
+          <div class="mt-5">
+            <span>Jetzt Namen eingeben und los gehts.</span>
+          </div>
+
+          <form class="mt-4">
+            <input
+              type="text"
+              name="nickname"
+              id="nickname"
+              class="form-control text-center"
+              placeholder="Nickname"
+            />
+            <div class="d-grid mt-2">
+              <button class="btn bg-vue2">Start</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+};
+</script>
+
+<style scoped></style>
---
+++ src/seed.js
@@ -0,0 +1,219 @@
+export const quizData = [
+  {
+    id: 1,
+    question: "Wie heißt der Raum im Flugzeug, in dem der Pilot sitzt?",
+    possibleAnswers: {
+      a: "Pitbull",
+      b: "Kombüse",
+      c: "Cockpit",
+    },
+    correctAnswer: "c",
+  },
+  {
+    id: 2,
+    question:
+      "Was steuert der Flugkapitän eines Passagierflugzeugs mit dem Gashebeln?",
+    possibleAnswers: {
+      a: "Die Fluglage",
+      b: "Die Leistung und die Geschwindigkeit der Triebwerke",
+      c: "Das Fahrwerk",
+    },
+    correctAnswer: "b",
+  },
+  {
+    id: 3,
+    question:
+      "Welches Tier ist symbolisch auf den Maschinen der deutschen Lufthansa zu sehen?",
+    possibleAnswers: {
+      a: "Eine Flugente",
+      b: "Ein Kranich",
+      c: "Ein Adler",
+    },
+    correctAnswer: "b",
+  },
+  {
+    id: 4,
+    question:
+      "Wie nennt man Flugzeuge, mit denen man auf dem Kopf fliegen oder Loopings drehen kann?",
+    possibleAnswers: {
+      a: "Kopffüßer",
+      b: "Kunstflugzeuge",
+      c: "Artistenjets",
+    },
+    correctAnswer: "b",
+  },
+  {
+    id: 5,
+    question:
+      "Was hilft dem Piloten, wenn er keine Sicht nach außen hat, zur Beurteilung der Lage des Flugzeugs?",
+    possibleAnswers: {
+      a: "Das Sprechfunkgerät",
+      b: "Die Stewardeß",
+      c: "Der künstliche Horizont",
+    },
+    correctAnswer: "c",
+  },
+  {
+    id: 6,
+    question:
+      "Wie heißt das Instrument, an dem der Pilot den Kurs ablesen kann?",
+    possibleAnswers: {
+      a: "Fahrtmesser",
+      b: "Wendekreisel",
+      c: "Kompaß",
+    },
+    correctAnswer: "c",
+  },
+  {
+    id: 7,
+    question:
+      "Segelflugzeuge können manchmal stundenlang mit Hilfe von warmen, aufsteigenden Luftmassen in der Luft bleiben. Wie nennt man diese warme Luft?",
+    possibleAnswers: {
+      a: "Föhn",
+      b: "Mistral",
+      c: "Thermik",
+    },
+    correctAnswer: "c",
+  },
+  {
+    id: 8,
+    question:
+      "Wie nennt man die Fluggeräte, mit denen man Raumschiffe ins Weltall schickt?",
+    possibleAnswers: {
+      a: "Raketen",
+      b: "Düsenjäger",
+      c: "Satelliten",
+    },
+    correctAnswer: "a",
+  },
+  {
+    id: 9,
+    question: "Die ersten Flugapparate hießen nicht Flugzeuge, sondern:",
+    possibleAnswers: {
+      a: "Luftikusse",
+      b: "Fluggleiter",
+      c: "Untertassen",
+    },
+    correctAnswer: "b",
+  },
+  {
+    id: 10,
+    question:
+      "Am 15.10.1783 hab François de Rozier als erster Mensch mit einem Heißluftballon vom Boden ab. Wer baute diesen Ballon?",
+    possibleAnswers: {
+      a: "Die Brüder Montgolfier",
+      b: "Die Gebrüder Wright",
+      c: "Die Gebrüder Grimm",
+    },
+    correctAnswer: "a",
+  },
+  {
+    id: 11,
+    question:
+      "Aus welchem Material war der erste Ballon der Brüder Montgolfier, der 1783 in die Lüfte stieg?",
+    possibleAnswers: {
+      a: "Aus Papier",
+      b: "Aus Gummi",
+      c: "Aus Tierhäuten",
+    },
+    correctAnswer: "a",
+  },
+  {
+    id: 12,
+    question:
+      "Die berühmtesten und größten Luftschiffe baute Ferdinand Graf von Zeppelin. Welche Länge hatte die Hindenburg?",
+    possibleAnswers: {
+      a: "110 m",
+      b: "191 m",
+      c: "247 m",
+    },
+    correctAnswer: "c",
+  },
+  {
+    id: 13,
+    question:
+      "Wieviel Kubikmeter Gas faßte der größte Zeppelin aller Zeiten, die Hindenburg?",
+    possibleAnswers: {
+      a: "50.000 m³",
+      b: "100.000m³",
+      c: "200.000m³",
+    },
+    correctAnswer: "c",
+  },
+  {
+    id: 14,
+    question:
+      "Wie weit flog der erste gesteuerte Motorflug der Welt - gebaut von den Gebrüdern Wright?",
+    possibleAnswers: {
+      a: "40 m",
+      b: "100 m",
+      c: "2.000 m",
+    },
+    correctAnswer: "a",
+  },
+  {
+    id: 15,
+    question:
+      "Woran orientierten sich die ersten Flugzeugpiloten, wenn sie längere Flüge machten?",
+    possibleAnswers: {
+      a: "An Geländemarken",
+      b: "An Radarstrahlen",
+      c: "An Markierungstürmen",
+    },
+    correctAnswer: "a",
+  },
+  {
+    id: 16,
+    question:
+      "In welchem Jahr fand der 1. Non-Stop-Flug über den Atlantik statt?",
+    possibleAnswers: {
+      a: "1903",
+      b: "1913",
+      c: "1919",
+    },
+    correctAnswer: "c",
+  },
+  {
+    id: 17,
+    question:
+      "Die Boeing 247D war der erste moderne Verkehrsflug der Welt. Wie lange brauchte sie, um die USA in der 30er Jahren zu überfliegen?",
+    possibleAnswers: {
+      a: "20 Stunden",
+      b: "30 Stunden",
+      c: "40 Stunden",
+    },
+    correctAnswer: "a",
+  },
+  {
+    id: 18,
+    question:
+      "Frühere Flugzeuge wurden in der Regel mit Porpellern angetrieben. Wie nennt man die Kraft, die durch einen Propeller entsteht?",
+    possibleAnswers: {
+      a: "Vorstrieb",
+      b: "Nachtrieb",
+      c: "Düsentrieb",
+    },
+    correctAnswer: "a",
+  },
+  {
+    id: 19,
+    question: "Seit wann gibt es Flugzeuge mit Düsenantrieb?",
+    possibleAnswers: {
+      a: "1920",
+      b: "1940",
+      c: "1960",
+    },
+    correctAnswer: "b",
+  },
+  {
+    id: 20,
+    question:
+      "Unter welchen allgemeinen Begriff faßt man alle Düsentriebwerke zusammen?",
+    possibleAnswers: {
+      a: "Daniels Antriebe",
+      b: "Strahltriebwerke",
+      c: "Lufttriebwerke",
+    },
+    correctAnswer: "b",
+  },
+];
