e5956bcba98cb3dea1f8b6f24333479424cd0cae
covoxS7 <lehmann.s7@gmx.de>
2022-12-05 00:15:02 +0100

    Layout überarbeitet

---
+++ src/layouts/TheEmptyLayout.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="container-fluid" :class="containerClasses">
+    <div class="row" :class="topRowClasses">
+      <div :class="middleColumnClasses">
+        <slot name="middleCol">
+          <h1>Mittlere Spalte</h1>
+        </slot>
+      </div>
+    </div>
+    <div class="row" :class="bottumRowClasses">
+      <div :class="leftColumnClasses">
+        <slot name="leftCol">
+          <h1>Linke Spalte</h1>
+        </slot>
+      </div>
+      <div :class="rightColumnClasses">
+        <slot name="rightCol">
+          <h1>Rechte Spalte</h1>
+        </slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheTwoColumnsLayout",
+  props: {
+    containerClass: {
+      type: String,
+    },
+    topRowClass: {
+      type: String,
+    },
+    bottumRowClass: {
+      type: String,
+    },
+    middleColumnClass: {
+      type: String,
+      default: "offset-1 col-10",
+    },
+    leftColumnClass: {
+      type: String,
+      default: "col-10 col-sm-5",
+    },
+    rightColumnClass: {
+      type: String,
+      default: "col-10 col-sm-5",
+    },
+    fullsize: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  computed: {
+    containerClasses() {
+      return [this.containerClass, this.fullsize ? "vh-100" : ""];
+    },
+    topRowClasses() {
+      return [this.topRowClass, this.fullsize ? "ht" : ""];
+    },
+    bottumRowClasses() {
+      return [this.bottumRowClass, this.fullsize ? "hb" : ""];
+    },
+    middleColumnClasses() {
+      return this.middleColumnClass;
+    },
+    leftColumnClasses() {
+      return this.leftColumnClass;
+    },
+    rightColumnClasses() {
+      return this.rightColumnClass;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.ht {
+  height: 35%;
+}
+.hb {
+  height: 65%;
+}
+
+@media (max-width: 992px) {
+  .ht {
+    height: 30%;
+  }
+  .hb {
+    height: 70%;
+  }
+}
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,39 +1,56 @@
 <template>
-  <div class="container-sm vh-100 bg-vue">
-    <div class="row h-100">
-      <div class="offset-1 col-10">
-        <div class="mt-5 text-center">
-          <div class="display-5">Flugzeug</div>
-          <hr />
-          <div class="display-1">Quiz</div>
-          <div class="mt-5">
-            <img src="@/assets/p47thunderbolt.svg" alt="" class="img-fluid" />
-          </div>
-          <div class="mt-5">
-            <span>Jetzt Namen eingeben und los gehts.</span>
-          </div>
-
-          <form class="mt-4">
-            <input
-              type="text"
-              name="nickname"
-              id="nickname"
-              class="form-control text-center"
-              placeholder="Nickname"
-            />
-            <div class="d-grid mt-2">
-              <button class="btn bg-vue2">Start</button>
-            </div>
-          </form>
-        </div>
+  <TheEmptyLayout
+    :fullsize="true"
+    :containerClass="'text-center bg-vue'"
+    :topRowClass="'align-items-center offset-lg-1 col-lg-10'"
+    :bottumRowClass="'align-items-center justify-content-center offset-lg-1 col-lg-10'"
+    :middleColumnClass="'offset-1 col-10 offset-lg-4 col-lg-4'"
+    :leftColumnClass="'col-10 col-lg-6'"
+    :rightColumnClass="'col-10 col-lg-4'"
+  >
+    <template #middleCol>
+      <div>
+        <div class="display-5">Flugzeug</div>
+        <hr />
+        <div class="display-1">Quiz</div>
       </div>
-    </div>
-  </div>
+    </template>
+    <template #leftCol>
+      <div>
+        <img
+          src="@/assets/p47thunderbolt.svg"
+          alt="Flugzeug"
+          class="img-fluid"
+        />
+      </div>
+    </template>
+    <template #rightCol>
+      <div class="mb-5">
+        <span>Jetzt Namen eingeben und los gehts.</span>
+        <form class="mt-4">
+          <input
+            type="text"
+            name="nickname"
+            id="nickname"
+            class="form-control text-center"
+            placeholder="Nickname"
+          />
+          <div class="d-grid mt-2">
+            <button class="btn bg-vue2">Start</button>
+          </div>
+        </form>
+      </div>
+    </template>
+  </TheEmptyLayout>
 </template>
 
 <script>
+import TheEmptyLayout from "@/layouts/TheEmptyLayout.vue";
 export default {
   name: "HomePage",
+  components: {
+    TheEmptyLayout,
+  },
 };
 </script>
 
